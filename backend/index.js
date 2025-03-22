db2fdf6455e537f02405cf21c53b7481911b2d81
pallavi-2303 <singh.pallavikumari14@gmail.com>
2025-03-23 02:04:13 +0530

    adding small changes for deploying

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,11 @@ import interviewRouter from "./routes/interview.route.js";
 import userAnswerRouter from "./routes/userAnswer.route.js"
 import resumeRouter from "./routes/resume.route.js";
 import coverLetterRouter from "./routes/coverLetter.route.js"
+import path from "path";
 dotenv.config();
 connectDB();
 const app=express();
+const _dirname=path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({extended:true}));
@@ -25,6 +27,10 @@ app.use("/userAnswer",userAnswerRouter);
 app.use("/resume",resumeRouter);
 app.use("/coverLetter",coverLetterRouter);
 const PORT=8000;
+app.use(express.static(path.join(_dirname,"/frontend/dist")));
+app.get('*',(_,res)=>{
+    res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
+})
 app.listen(PORT,()=>{
  console.log(`Server listen on http://localhost${PORT}`);   
 })
